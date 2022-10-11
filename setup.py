dc9417596c5d84e0d674face236566c4853a4781
HyperSine <hypersine.git@outlook.com>
2022-10-11 15:25:40 +0800

    remove -std, /std flags when compiling .c files
    Signed-off-by: HyperSine <hypersine.git@outlook.com>

--- setup.py
+++ setup.py
@@ -1,8 +1,25 @@
 #!/usr/bin/env python3
+import functools
 from setuptools import setup
 from pybind11.setup_helpers import Pybind11Extension, build_ext, ParallelCompile, naive_recompile
 
 ParallelCompile(needs_recompile = naive_recompile).install()
+
+class Pybind11BuildExtOverride(build_ext):
+
+    def build_extensions(self):
+        def _compile_wrap(original_compile):
+            @functools.wraps(original_compile)
+            def _custom_compile(obj, src, ext, cc_args, extra_postargs, pp_opts):
+                # if `src` is .c file
+                # remove `-std=` or `/std:` flags from `extra_postargs`
+                if src.endswith('.c'):
+                    extra_postargs = [ *filter(lambda s: not s.startswith('-std=') and not s.startswith('/std:'), extra_postargs) ]
+                return original_compile(obj, src, ext, cc_args, extra_postargs, pp_opts)
+            return _custom_compile
+
+        self.compiler._compile = _compile_wrap(self.compiler._compile)
+        super().build_extensions()
 
 authors = {
     'Xiao': ('Aiyu Xiao', 'hypersine.git@outlook.com')
@@ -27,5 +44,5 @@ if __name__ == '__main__':
                 include_dirs = [ 'src/jbigkit/libjbig' ]
             )
         ],
-        cmdclass = { 'build_ext': build_ext },
+        cmdclass = { 'build_ext': Pybind11BuildExtOverride },
     )
